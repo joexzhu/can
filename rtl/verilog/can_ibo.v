cf7a3b33294c3f7379c9180741c991111d46a636
Ming Ze <zhuxun_cn@yahoo.com>
2025-07-30 10:22:07 +0800

    Refactor output signal naming from 'do' to 'd_o' to avoid conflicting with keyword

--- rtl/verilog/can_ibo.v
+++ rtl/verilog/can_ibo.v
@@ -66,19 +66,19 @@
 module can_ibo
 ( 
   di,
-  do
+  d_o
 );
 
 input   [7:0] di;
-output  [7:0] do;
+output  [7:0] d_o;
 
-assign do[0] = di[7];
-assign do[1] = di[6];
-assign do[2] = di[5];
-assign do[3] = di[4];
-assign do[4] = di[3];
-assign do[5] = di[2];
-assign do[6] = di[1];
-assign do[7] = di[0];
+assign d_o[0] = di[7];
+assign d_o[1] = di[6];
+assign d_o[2] = di[5];
+assign d_o[3] = di[4];
+assign d_o[4] = di[3];
+assign d_o[5] = di[2];
+assign d_o[6] = di[1];
+assign d_o[7] = di[0];
 
 endmodule
